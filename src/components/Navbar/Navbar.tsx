7c751387118b14205947cf500266301895715259
andbudha <andreibartov@gmail.com>
2024-07-13 13:57:12 +0200

    Navbar created&styled

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React from 'react';
+import styles from './Navbar.module.scss';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <div className={styles.navbar_main_box}>
+      <ul className={styles.navbar_link_box}>
+        <Link
+          className={`${styles.navbar_link} ${
+            pathname === '/' && styles.active_link
+          }`}
+          href={'/'}
+        >
+          Home
+        </Link>
+        <Link
+          className={`${styles.navbar_link} ${
+            pathname === '/characters' && styles.active_link
+          }`}
+          href={'/characters'}
+        >
+          Characters
+        </Link>
+        <Link
+          className={`${styles.navbar_link} ${
+            pathname === '/episodes' && styles.active_link
+          }`}
+          href={'/episodes'}
+        >
+          Episodes
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
