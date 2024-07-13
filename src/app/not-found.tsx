3723e2ab2eaa1d2ee9246a7b0af67b2bf4a38933
andbudha <andreibartov@gmail.com>
2024-07-13 14:00:39 +0200

    NotFound page added&styled

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './not-found.module.scss';
+
+const NotFound = () => {
+  return (
+    <div className={styles.not_found_main_box}>
+      <h1>404 | Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
