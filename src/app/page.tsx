3034ccb6ad7c3b29cf6f6c2489652d78c2995e3b
andbudha <andreibartov@gmail.com>
2024-07-14 10:20:35 +0200

    HomePage updated&restyled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,30 @@ export default function Home() {
 
   return (
     <main className={styles.home_main_box}>
-      <div>Home Page</div>
+      <h2>Home-Page</h2>
+      <p>
+        I have recenty started learning{' '}
+        <span className={styles.home_page_span}>Next.js</span> and I really hope
+        this page gets{' '}
+        <span className={styles.home_page_span}>statically rendered</span> by
+        default.
+      </p>
+      <p>
+        I am not sure if the{' '}
+        <span className={styles.home_page_span}>Characters Page</span> gets
+        rendered on the{' '}
+        <span className={styles.home_page_span}>Server-Side</span> as I use
+        state variables in there and I had to specify to use this page{' '}
+        <span className={styles.home_page_span}>'as client'</span>. I will have
+        to ask our mentor <span className={styles.home_page_span}>Raul</span>,
+        during the prsenetation of this{' '}
+        <span className={styles.home_page_span}>itsy-bitsy</span> project, that
+        if we specify that a page should be used{' '}
+        <span className={styles.home_page_span}>'as client</span>, this page
+        will no longer get rendered on the{' '}
+        <span className={styles.home_page_span}>Server-Side</span> but on the{' '}
+        <span className={styles.home_page_span}>Client-Side</span> instead.
+      </p>
     </main>
   );
 }
